696ebeb9b0bebb7fc0b1a81c001880c0223f96e2
Elad Nava <eladnava@gmail.com>
2015-10-24 00:58:37 +0300

    Doze Mode & App Standby Compatibility
    Support Android 6.0 (Marshmallow) devices by adding a dialog that asks
    the user to disable doze mode explicitly for RedAlert, so that rocket
    alerts may be received in real time (through the secondary push
    notification gateway).
    
    GCM push notifications are sent as high-priority anyway, but we want the
    secondary push gateway to work as well, just in case.

--- app/src/main/java/com/red/alert/activities/Main.java
+++ app/src/main/java/com/red/alert/activities/Main.java
@@ -38,6 +38,7 @@ import com.red.alert.model.res.VersionInfo;
 import com.red.alert.ui.adapters.AlertAdapter;
 import com.red.alert.ui.compatibility.ProgressDialogCompat;
 import com.red.alert.ui.dialogs.AlertDialogBuilder;
+import com.red.alert.ui.dialogs.custom.CompatibilityDialogs;
 import com.red.alert.ui.dialogs.custom.LocationDialogs;
 import com.red.alert.ui.dialogs.custom.UpdateDialogs;
 import com.red.alert.ui.localization.rtl.RTLSupport;
@@ -45,6 +46,7 @@ import com.red.alert.ui.notifications.AppNotifications;
 import com.red.alert.utils.bugtracking.SplunkMINT;
 import com.red.alert.utils.caching.Singleton;
 import com.red.alert.utils.communication.Broadcasts;
+import com.red.alert.utils.compatibility.DozeCompatibility;
 import com.red.alert.utils.feedback.Volume;
 import com.red.alert.utils.formatting.StringUtils;
 import com.red.alert.utils.integration.WhatsApp;
@@ -85,9 +87,6 @@ public class Main extends AppCompatActivity
 
         // Initialize app UI
         initializeUI();
-
-        // Got any dialogs to display?
-        showImportantDialogs();
 
         // Start polling for recent alerts
         pollRecentAlerts();
@@ -254,6 +253,9 @@ public class Main extends AppCompatActivity
         // Reload alerts manually
         reloadRecentAlerts();
 
+        // Got any dialogs to display?
+        showImportantDialogs();
+
         // Support for RTL languages
         RTLSupport.mirrorActionBar(this);
 
@@ -300,7 +302,17 @@ public class Main extends AppCompatActivity
             // Register async
             new RegisterPushAsync().execute();
 
-            // Avoid checking for updates now (to avoid duplicate dialog)
+            // Avoid other checks (to avoid duplicate dialogs)
+            return;
+        }
+
+        // Compatibility with Doze and App Standby (Android 6.0+)
+        if (DozeCompatibility.shouldDisableBatteryOptimizations(this))
+        {
+            // Tell user about Doze mode what to do next
+            CompatibilityDialogs.showDozeDialog(this);
+
+            // Avoid other checks (to avoid duplicate dialogs)
             return;
         }
 
