07f34c904cc0584c30ee7c4e1b00537bf08788c1
Elad Nava <eladnava@gmail.com>
2016-03-24 01:13:36 +0200

    Update development API port number

--- app/src/main/java/com/red/alert/config/API.java
+++ app/src/main/java/com/red/alert/config/API.java
@@ -3,7 +3,7 @@ package com.red.alert.config;
 public class API
 {
     // RedAlert API URL (no trailing slash)
-    //public static String API_ENDPOINT = "http://192.168.0.100:81";
+    //public static String API_ENDPOINT = "http://192.168.0.100:3000";
     public static String API_ENDPOINT = "https://redalert.me";
 
     // Define platform to be sent by our API
