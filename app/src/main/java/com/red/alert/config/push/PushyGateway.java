399b619632d1ae16d4b38de0908fbfe1b44a0aa5
Elad Nava <eladnava@gmail.com>
2016-11-19 15:06:09 +0200

    Update to Pushy SDK 1.0.19

--- app/src/main/java/com/red/alert/config/push/PushyGateway.java
+++ app/src/main/java/com/red/alert/config/push/PushyGateway.java
@@ -2,5 +2,5 @@ package com.red.alert.config.push;
 
 public class PushyGateway {
     // Set custom heartbeat interval (in ms) for the underlying MQTT socket
-    public static int SOCKET_HEARTBEAT_INTERVAL = 1000 * 60 * 3;
+    public static int SOCKET_HEARTBEAT_INTERVAL = 60 * 3;
 }
