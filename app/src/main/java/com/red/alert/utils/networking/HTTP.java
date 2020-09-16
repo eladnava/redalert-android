8491bb31be8e14a81fe99946859577e72d11396e
Elad Nava <eladnava@gmail.com>
2020-09-16 11:42:13 -0400

    Migrate from Apache HTTP to HttpsUrlConnection

--- app/src/main/java/com/red/alert/utils/networking/HTTP.java
+++ app/src/main/java/com/red/alert/utils/networking/HTTP.java
@@ -1,69 +1,165 @@
 package com.red.alert.utils.networking;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.util.EntityUtils;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class HTTP {
-    public static String get(String URL) throws Exception {
-        // Get custom http client
-        DefaultHttpClient client = getHTTPClient();
+    public static String get(String urlString) throws Exception {
+        // Create request
+        HttpsURLConnection httpConnection = null;
 
-        // Create GET request
-        HttpGet request = new HttpGet(URL);
+        try {
+            // Convert URL into object
+            URL url = new URL(urlString);
 
-        // Execute the request
-        HttpResponse response = client.execute(request, new BasicHttpContext());
+            // Prepare request
+            httpConnection = (HttpsURLConnection) url.openConnection();
 
-        // Return response as string
-        String responseText = EntityUtils.toString(response.getEntity());
+            // Set general properties
+            httpConnection.setUseCaches(false);
+            httpConnection.setRequestMethod("GET");
 
-        // Failed?
-        if (response.getStatusLine() == null || response.getStatusLine().getStatusCode() != 200) {
-            // Throw it out
-            throw new Exception(response.getStatusLine().toString() + "\n" + responseText);
+            // Execute request
+            httpConnection.connect();
+
+            // Execute request and get response code
+            int statusCode = httpConnection.getResponseCode();
+
+            // Prepare input stream
+            InputStream inputStream;
+
+            // 200 OK?
+            if (statusCode == 200) {
+                // Use standard stream
+                inputStream = httpConnection.getInputStream();
+            }
+            else {
+                // Use error stream
+                inputStream = httpConnection.getErrorStream();
+            }
+
+            // Prepare buffered reader
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            // Prepare response buffer holder
+            StringBuffer response = new StringBuffer();
+
+            // Temporary line placeholder
+            String tmpLine;
+
+            // Read from reader until end of stream
+            while ((tmpLine = reader.readLine()) != null) {
+                // Append line
+                response.append(tmpLine);
+            }
+
+            // Close reader
+            reader.close();
+
+            // Error?
+            if (statusCode != 200) {
+                // Throw it out
+                throw new Exception("HTTP GET error: " + statusCode + "\n" + response.toString());
+            }
+
+            // Return response as string
+            return response.toString();
+        } catch (Exception exc) {
+            // Throw connectivity exception
+            throw new Exception(exc.toString() + exc.getStackTrace());
         }
-
-        // We're good
-        return responseText;
+        finally {
+            // Got an open connection?
+            if (httpConnection != null) {
+                // Close it to avoid leaking resources
+                httpConnection.disconnect();
+            }
+        }
     }
 
-    public static String post(String url, String json) throws Exception {
-        // Get custom http client
-        HttpClient client = new DefaultHttpClient();
+    public static String post(String urlString, String json) throws Exception {
+        // Create request
+        HttpsURLConnection httpConnection = null;
 
-        // Create post request
-        HttpPost request = new HttpPost(url);
+        try {
+            // Convert URL into object
+            URL url = new URL(urlString);
 
-        // Set content type to JSON
-        request.addHeader("Content-Type", "application/json");
+            // Prepare request
+            httpConnection = (HttpsURLConnection) url.openConnection();
 
-        // Send JSON as string
-        request.setEntity(new StringEntity(json));
+            // Set JSON headers
+            httpConnection.setRequestProperty("Accept", "application/json");
+            httpConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
 
-        // Execute the request
-        HttpResponse response = client.execute(request, new BasicHttpContext());
+            // Set general properties
+            httpConnection.setDoInput(true);
+            httpConnection.setDoOutput(true);
+            httpConnection.setUseCaches(false);
+            httpConnection.setRequestMethod("POST");
 
-        // Return response as string
-        String responseText = EntityUtils.toString(response.getEntity());
+            // Send JSON post body
+            OutputStream outputStream = httpConnection.getOutputStream();
+            outputStream.write(json.getBytes("UTF-8"));
+            outputStream.close();
 
-        // Failed?
-        if (response.getStatusLine() == null || response.getStatusLine().getStatusCode() != 200) {
-            // Throw it out
-            throw new Exception(response.getStatusLine().toString() + "\n" + responseText);
+            // Execute request and get response code
+            int statusCode = httpConnection.getResponseCode();
+
+            // Prepare input stream
+            InputStream inputStream;
+
+            // 200 OK?
+            if (statusCode == 200) {
+                // Use standard stream
+                inputStream = httpConnection.getInputStream();
+            }
+            else {
+                // Use error stream
+                inputStream = httpConnection.getErrorStream();
+            }
+
+            // Prepare buffered reader
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            // Prepare response buffer holder
+            StringBuffer response = new StringBuffer();
+
+            // Temporary line placeholder
+            String tmpLine;
+
+            // Read from reader until end of stream
+            while ((tmpLine = reader.readLine()) != null) {
+                // Append line
+                response.append(tmpLine);
+            }
+
+            // Close reader
+            reader.close();
+
+            // Error?
+            if (statusCode != 200) {
+                // Throw it out
+                throw new Exception("HTTP POST error: " + statusCode + "\n" + response.toString());
+            }
+
+            // Return response as string
+            return response.toString();
+        } catch (Exception exc) {
+            // Throw connectivity exception
+            throw new Exception(exc.toString() + exc.getStackTrace());
         }
-
-        // We're good
-        return responseText;
-    }
-
-    public static DefaultHttpClient getHTTPClient() {
-        // Return default client
-        return new DefaultHttpClient();
+        finally {
+            // Got an open connection?
+            if (httpConnection != null) {
+                // Close it to avoid leaking resources
+                httpConnection.disconnect();
+            }
+        }
     }
 }
