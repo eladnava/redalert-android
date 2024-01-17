7b73a9e1feccb3472e6cc676313bec2d037b6557
Elad Nava <eladnava@gmail.com>
2024-01-17 09:10:00 -0500

    API: Remove 'ipv4.redalert.me' endpoint

--- app/src/main/java/com/red/alert/config/API.java
+++ app/src/main/java/com/red/alert/config/API.java
@@ -2,7 +2,7 @@ package com.red.alert.config;
 
 public class API {
     // RedAlert API Endpoint(s)
-    public static String[] ENDPOINTS = {"https://ipv4.redalert.me", "https://redalert.me", "https://api.redalert.me", "https://api.tzevaadom.app", "https://tzevaadom.app"};
+    public static String[] ENDPOINTS = {"https://redalert.me", "https://api.redalert.me", "https://api.tzevaadom.app", "https://tzevaadom.app"};
 
     // Define platform to be sent by our API
     public static String PLATFORM_IDENTIFIER = "android";
