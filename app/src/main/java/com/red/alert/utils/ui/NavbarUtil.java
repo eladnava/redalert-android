089a22ee2e60e294a7543c7e1065b3548fc6be31
Elad Nava <eladnava@gmail.com>
2026-01-21 15:58:36 -0300

    NavbarUtil: Fix settings UI overlay bug (Android 15 only)

--- app/src/main/java/com/red/alert/utils/ui/NavbarUtil.java
+++ app/src/main/java/com/red/alert/utils/ui/NavbarUtil.java
@@ -1,9 +1,13 @@
 package com.red.alert.utils.ui;
 
 import android.app.Activity;
+import android.os.Build;
 import android.view.View;
 
 import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.red.alert.R;
 
@@ -14,9 +18,31 @@ public class NavbarUtil {
 
         // Set fits system window & background color (so it appears below navbar)
         root.setFitsSystemWindows(true);
+
+        // Fix Android 15 navbar overlay bug
+        if (Build.VERSION.SDK_INT == 35) {
+            // Wait for insets
+            ViewCompat.setOnApplyWindowInsetsListener(root, (v, insets) -> {
+                // Get system bar insets
+                Insets bars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+
+                // Apply top & bottom padding
+                v.setPadding(
+                        v.getPaddingLeft(),
+                        bars.top,
+                        v.getPaddingRight(),
+                        bars.bottom
+                );
+
+                // Override default insets
+                return insets;
+            });
+        }
+
+        // Override background color
         root.setBackgroundColor(ContextCompat.getColor(activity, R.color.colorPrimaryDark));
 
-        // Set list view background color
+        // Override list view background color
         activity.findViewById(android.R.id.list).setBackgroundColor(ContextCompat.getColor(activity, R.color.colorBackground));
     }
 }
